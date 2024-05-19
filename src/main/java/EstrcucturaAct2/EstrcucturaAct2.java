b62b459d4f526578dafdc0bc8545eb929da118a0
Juan Diego <Juan Diego@Juann>
2024-05-19 04:58:22 -0500

    Prueba

---
+++ src/main/java/EstrcucturaAct2/EstrcucturaAct2.java
@@ -0,0 +1,15 @@
+
+package EstrcucturaAct2;
+
+public class EstrcucturaAct2 {
+
+    public static void main(String[] args) {
+      
+        
+        
+        
+        
+        
+        
+    }
+}
